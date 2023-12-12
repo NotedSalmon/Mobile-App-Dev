000e277342cf7a526458b2c9239737eafcdc2ec9
Flavio <94856026+NotedSalmon@users.noreply.github.com>
2023-12-12 16:26:54 +0000

    Created base for Max and added comments.

---
+++ app/src/main/java/com/example/wagonersexperts/Admin/Admin_DataFiles.kt
@@ -0,0 +1,10 @@
+package com.example.wagonersexperts.Admin
+
+data class Admin_DataFiles(
+    val id: Int
+) {
+
+    override fun toString(): String{
+        return "Check Customer_DataFiles for Reference"
+    }
+}
