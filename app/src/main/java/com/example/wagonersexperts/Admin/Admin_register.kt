522d2cad85970acbc16e843b6992ef4b02cacbba
Max <max.jacobs.flighta10@gmail.com>
2023-12-20 21:15:35 +0000

    SHA-encryption

---
+++ app/src/main/java/com/example/wagonersexperts/Admin/Admin_register.kt
@@ -0,0 +1,6 @@
+package com.example.wagonersexperts.Admin
+
+import androidx.appcompat.app.AppCompatActivity
+class Admin_register : AppCompatActivity() {
+
+}
--- app/src/main/java/com/example/wagonersexperts/extra/SHAEncryption.kt
+++ app/src/main/java/com/example/wagonersexperts/extra/SHAEncryption.kt
@@ -1,13 +1,14 @@
-package Extra
+package com.example.wagonersexperts.extra
 
 import java.security.MessageDigest
 
 //https://gist.github.com/lovubuntu/164b6b9021f5ba54cefc67f60f7a1a25
-class SHAEncryption {
-    fun SHA(): String {
-        val bytes = this.toString().toByteArray()
+object SHAEncryption {
+    fun shaEncrypt(info : String): String {
+        val bytes = info.toByteArray()
         val md = MessageDigest.getInstance("SHA-256")
         val digest = md.digest(bytes)
+
         return digest.fold("") { str, it -> str + "%02x".format(it) }
     }
 
