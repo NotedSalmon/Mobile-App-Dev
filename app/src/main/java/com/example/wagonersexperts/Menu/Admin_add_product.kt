a8aeaf8c2359de042108474773a8eb6af6febea3
Max Jacobs <P2597948@my365.dmu.ac.uk>
2024-01-07 04:52:11 +0000

    commit

--- app/src/main/java/com/example/wagonersexperts/Menu/Admin_add_product.kt
+++ app/src/main/java/com/example/wagonersexperts/Menu/Admin_add_product.kt
@@ -1,23 +1,23 @@
-package com.example.wagonersexperts.Menu
-
-import android.app.Activity
-import android.content.Intent
-import android.media.Image
-import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import com.example.wagonersexperts.R
-import kotlinx.coroutines.selects.select
-
-class Admin_add_product : AppCompatActivity() {
+ : AppCompatActivity() {
     private val imageRequest = 1
     var imageToByte: ByteArray? = null
+    package com.example.wagonersexperts.Menu
 
+    import android.app.Activity
+    import android.content.Intent
+    import android.media.Image
+    import android.os.Bundle
+    import android.view.View
+    import android.widget.Button
+    import android.widget.EditText
+    import android.widget.ImageView
+    import android.widget.Toast
+    import androidx.activity.result.contract.ActivityResultContracts
+    import androidx.appcompat.app.AppCompatActivity
+    import com.example.wagonersexperts.R
+    import kotlinx.coroutines.selects.select
+
+    class Admin_add_product
     val dbHelper: Menu_DBHelper = Menu_DBHelper(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
